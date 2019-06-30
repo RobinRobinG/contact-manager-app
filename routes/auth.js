dd8e9302571fa0c3952b4487c46058b1b7c5c890
ruobing guan <ruobingguan@ruobings-MacBook-Pro.local>
2019-06-30 16:34:53 -0700

    setup express server

---
+++ routes/auth.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+// @route   GET  api/auth
+// @desc    Get Login user
+// @access  Private
+router.get('/', (req, res) => {
+    res.send('Get logged in  user');
+});
+
+// @route   POST  api/auth
+// @desc    Auth user & get token
+// @access  Public
+router.post('/', (req, res) => {
+    res.send('log  in  user');
+});
+
+
+module.exports = router;
