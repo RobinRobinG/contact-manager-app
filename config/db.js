b28a57d5e516c039edf6b290f61f8db593626574
ruobing guan <ruobingguan@ruobings-MacBook-Pro.local>
2019-07-07 01:28:07 -0700

    WIP

--- config/db.js
+++ config/db.js
@@ -3,17 +3,17 @@ const config = require('config');
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db, {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useFindAndModify: false
-        });
-        console.log('MongoDB Conntected');
-    } catch (error) {
-        console.error(error.message);
-        process.exit(1);
-    }
-}
+	try {
+		await mongoose.connect(db, {
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+		});
+		console.log('MongoDB connected');
+	} catch (error) {
+		console.error(error.message);
+		process.exit(1);
+	}
+};
 
 module.exports = connectDB;
